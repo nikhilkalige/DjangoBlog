8a429dfe155bffb518455a049e8c821aaf60b534
Lonewolf <nikhilkalige@gmail.com>
2013-09-25 22:46:21 +0530

    Cleanup done Added generic views RSS icon added Minor bug fixes related to url

--- djangoblog/blogengine/models.py
+++ djangoblog/blogengine/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=40, unique=True)
@@ -14,8 +14,7 @@ class Category(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return "/categories/%s" % (self.slug)
-
+        return "/categories/%s/" % self.slug
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -25,12 +24,14 @@ class Post(models.Model):
     author = models.ForeignKey(User)
     categories = models.ManyToManyField(Category, blank=True, null=True, through='CategoryToPost')
 
+    class Meta:
+        ordering = ["-pub_date"]
+
     def __unicode__(self):
         return self.title
 
     def get_absolute_url(self):
-        return "%s/%s/%s" % (self.pub_date.year, self.pub_date.month, self.slug)
-
+        return "/%s/%s/%s/" % (self.pub_date.year, self.pub_date.month, self.slug)
 
 class CategoryToPost(models.Model):
     post = models.ForeignKey(Post)
--- djangoblog/blogengine/views.py
+++ djangoblog/blogengine/views.py
@@ -1,54 +1,40 @@
+# Create your views here.
 from django.shortcuts import render_to_response
 from django.core.paginator import Paginator, EmptyPage
 from django.template import RequestContext
+from blogengine.models import Post, Category
 from django.contrib.syndication.views import Feed
 from django.contrib.flatpages.models import FlatPage
-
-from blogengine.models import Post, Category
-
-
-def getPosts(request, selected_page=1):
-    # get all blog posts
-    posts = Post.objects.all().order_by('-pub_date')
-
-    # add pagination
-    pages = Paginator(posts, 5)
-
-    try:
-        returned_page = pages.page(selected_page)
-    except EmptyPage:
-        returned_page = pages.page(pages.num_pages)
-
-    # display all posts
-    return render_to_response('posts.html', {'posts': returned_page.object_list, 'page': returned_page, })
-
 
 def getPost(request, postSlug):
     # Get specified post
     post = Post.objects.filter(slug=postSlug)
 
     # Display specified post
-    return render_to_response('single.html', {'posts': post}, context_instance=RequestContext(request))
-
+    return render_to_response('single.html', { 'posts':post}, context_instance=RequestContext(request))
 
 def getCategory(request, categorySlug, selected_page=1):
     # Get specified category
-    posts = Post.get.objects.all().order_by('-pub_date')
+    posts = Post.objects.all().order_by('-pub_date')
     category_posts = []
     for post in posts:
         if post.categories.filter(slug=categorySlug):
             category_posts.append(post)
 
+    # Add pagination
     pages = Paginator(category_posts, 5)
 
+    # Get the category
     category = Category.objects.filter(slug=categorySlug)[0]
 
+    # Get the specified page
     try:
         returned_page = pages.page(selected_page)
     except EmptyPage:
         returned_page = pages.page(pages.num_pages)
-    return render_to_response('category.html', {'posts': returned_page.object_list, 'page': returned_page, 'category': category})
 
+    # Display all the posts
+    return render_to_response('category.html', { 'posts': returned_page.object_list, 'page': returned_page, 'category': category})
 
 class PostsFeed(Feed):
     title = "My Django Blog posts"
